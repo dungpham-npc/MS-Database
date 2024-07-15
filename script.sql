17a2691ab5477995c6494fda5d66d2008b7569d7
ThePenitentOne07 <144657390+ThePenitentOne07@users.noreply.github.com>
2024-07-15 12:40:41 +0700

    insert data into milk_product

--- script.sql
+++ script.sql
@@ -333,15 +333,15 @@ INSERT INTO post (user_id, title, content, date_created, user_comment, visibilit
 
 
 
-INSERT INTO milk_product (category_id, post_id, product_name, product_description, product_image, quantity_in_stock, price, manu_date, expi_date, status) VALUES
-(1, 1, 'Organic Whole Milk', 'Fresh and healthy organic whole milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/8b3c3bcc-22f8-4546-bdcd-9a30f06f6cdd.jpg?alt=media', 100, 3.99, '2024-01-01', '2024-07-01', TRUE),
-(1, 2, 'Organic Skim Milk', 'Low-fat organic skim milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/d652673a-8a61-4d07-82ef-f6155a249efe.webp?alt=media', 150, 3.49, '2024-01-01', '2024-07-01', TRUE),
-(1, 3, 'Organic 2% Milk', 'Organic milk with 2% fat content.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/353a2dc5-6c34-47d9-93f2-11b88e45000c.jpg?alt=media', 120, 3.79, '2024-01-01', '2024-07-01', TRUE),
-(2, 4, 'Chocolate Milk', 'Delicious chocolate flavored milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/638d6717-c3c3-4810-90da-5baf9703e584.jpg?alt=media', 200, 2.99, '2024-01-01', '2024-06-01', TRUE),
-(2, 5, 'Strawberry Milk', 'Sweet and tasty strawberry milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/5348af57-d662-4c5d-9999-caa4a8044c9e.jpg?alt=media', 180, 2.99, '2024-01-01', '2024-06-01', TRUE),
-(2, 1, 'Vanilla Milk', 'Smooth vanilla flavored milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/9ff0c23d-33af-4e7b-bd43-f19306abcd85.jpg?alt=media', 170, 2.99, '2024-01-01', '2024-06-01', TRUE),
-(3, 2, 'Almond Milk', 'Healthy almond milk, dairy-free.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/665ffad7-919e-436b-937d-eafd9187e578.jpg?alt=media', 140, 3.99, '2024-01-01', '2024-12-01', TRUE),
-(3, 3, 'Soy Milk', 'Nutritious soy milk, dairy-free.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/d135706b-8825-4225-b2d9-d9dbf4044741.jpg?alt=media', 130, 3.49, '2024-01-01', '2024-12-01', TRUE);
+INSERT INTO milk_product (category_id, post_id, product_name, product_description, product_image, quantity_in_stock, price, manu_date, expi_date, delete_status, visibility_status) VALUES
+(1, 1, 'Organic Whole Milk', 'Fresh and healthy organic whole milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/8b3c3bcc-22f8-4546-bdcd-9a30f06f6cdd.jpg?alt=media', 100, 3.99, '2024-01-01', '2024-07-01', TRUE, TRUE),
+(1, 2, 'Organic Skim Milk', 'Low-fat organic skim milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/d652673a-8a61-4d07-82ef-f6155a249efe.webp?alt=media', 150, 3.49, '2024-01-01', '2024-07-01', TRUE, TRUE),
+(1, 3, 'Organic 2% Milk', 'Organic milk with 2% fat content.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/353a2dc5-6c34-47d9-93f2-11b88e45000c.jpg?alt=media', 120, 3.79, '2024-01-01', '2024-07-01', TRUE, TRUE),
+(2, 4, 'Chocolate Milk', 'Delicious chocolate flavored milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/638d6717-c3c3-4810-90da-5baf9703e584.jpg?alt=media', 200, 2.99, '2024-01-01', '2024-06-01', TRUE, TRUE),
+(2, 5, 'Strawberry Milk', 'Sweet and tasty strawberry milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/5348af57-d662-4c5d-9999-caa4a8044c9e.jpg?alt=media', 180, 2.99, '2024-01-01', '2024-06-01', TRUE, TRUE),
+(2, 1, 'Vanilla Milk', 'Smooth vanilla flavored milk.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/9ff0c23d-33af-4e7b-bd43-f19306abcd85.jpg?alt=media', 170, 2.99, '2024-01-01', '2024-06-01', TRUE, TRUE),
+(3, 2, 'Almond Milk', 'Healthy almond milk, dairy-free.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/665ffad7-919e-436b-937d-eafd9187e578.jpg?alt=media', 140, 3.99, '2024-01-01', '2024-12-01', TRUE, TRUE),
+(3, 3, 'Soy Milk', 'Nutritious soy milk, dairy-free.', 'https://firebasestorage.googleapis.com/v0/b/firebase-service-427505.appspot.com/o/d135706b-8825-4225-b2d9-d9dbf4044741.jpg?alt=media', 130, 3.49, '2024-01-01', '2024-12-01', TRUE, TRUE);
 -- (3, 4, 'Coconut Milk', 'Refreshing coconut milk, dairy-free.', 'coconut_milk.jpg', 110, 3.89, '2024-01-01', '2024-12-01', TRUE),
 -- (1, 5, 'Organic Lactose-Free Milk', 'Organic milk without lactose.', 'organic_lactose_free_milk.jpg', 80, 4.49, '2024-01-01', '2024-07-01', TRUE),
 -- (2, 1, 'Banana Milk', 'Yummy banana flavored milk.', 'banana_milk.jpg', 160, 2.99, '2024-01-01', '2024-06-01', TRUE),
