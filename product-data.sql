19ba6a3973e93f2f196d2878360492fe06613f36
Hoang Hai Hau <hhhau1910@gmail.com>
2024-07-23 17:47:32 +0700

    add new file sql to add sample data

---
+++ product-data.sql
@@ -0,0 +1,10 @@
+use milkstore;
+
+INSERT INTO `milkstore`.`milk_product_category`
+(`category_name`)
+VALUES
+('Sữa bột pha sẵn'),
+('Sữa bầu'),
+('Sữa bột'),
+('Sữa tăng cân cho bé'),
+('Sữa tăng chiều cao');
